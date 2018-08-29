992ba7f746f9239e8840d9603084c9d333a874a4
tercel2018 <924309827@qq.com>
2018-08-29 23:24:44 +0800

    页码

--- dao/position_dao.js
+++ dao/position_dao.js
@@ -4,6 +4,17 @@ const PositionDao = {
     save(positioninfo){
         return new Position(positioninfo).save();
     },
+    //安页查找职位信息
+    findByPage(page){
+        //假定每页显示的5条数据
+        const pageSize = 5;
+        //查询
+        const query = Position.find(); //查询结果集
+        const count = query.count()//纪录条数
+        const Position = Position.find().skip((page-1)*pageSize).limit(pageSize);
+        return {count,Position}
+
+    },
     updata(){
         
     },
--- services/position_services.js
+++ services/position_services.js
@@ -18,6 +18,20 @@ const PositionService = {
             .catch(err=>{
                 res.json({res_code:-1,res_error:err,res_body:{}})
             });
+    },
+    //分页查询职位
+    letByPage(req,res,next){
+        //获取当前查询的页码
+        let {page} = req.query;
+        page = page ||1;
+        //调用数据库查询方法
+        PositionDao
+            .findByPage(page)
+            .then(data =>{
+                console.log(data);
+            })
+            .catch();
+        
     }
 }
 module.exports = PositionService;
